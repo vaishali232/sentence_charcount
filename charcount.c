927cd67d39ab3f1bb8424311d6c193f533ee1c26
vaishali232 <51689354+vaishali232@users.noreply.github.com>
2019-08-08 13:08:24 +0530

    Create charcount.c

---
+++ charcount.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char s[10000];
+long long int n,count=0,i;
+scanf("%[^\n]",&s);
+n=strlen(s);
+for(i=0;i<n;i++)
+{
+    if(s[i]!=' ')
+    {
+        count++;
+    }
+}
+printf("%lld",count);
